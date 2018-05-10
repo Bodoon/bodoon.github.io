f01c492b955d4d8aab2e8523fbb64fd6a2893b0f
Bodoon <witchertheavenger@gmail.com>
2018-05-10 10:46:23 +0300

    tr-logic test deleted

--- tr-logic-test/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {	
-	$('#open--left').click(function() {
-		$('.aside-left').stop().animate({width: 'toggle', display: 'block'}, 250);
-		});
-	$('#open--right').click(function() {
-		$('.aside-right').stop().animate({width: 'toggle', display: 'block'}, 250);
-		});	
-	});
