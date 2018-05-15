a55d37b8a3c73768ea9e42b3db8cf211fea253c4
Bodoon <witchertheavenger@gmail.com>
2018-05-15 19:32:09 +0300

    very-basic-and-simple-table-data-editor

---
+++ hexa1/js/main.js
@@ -0,0 +1,127 @@
+$(document).ready(function ($) {
+
+    //check if table is in localstorage
+    if (localStorage.getItem('localTable') != undefined) {
+        document.getElementById('table-body').insertAdjacentHTML('beforeEnd', localStorage.getItem('localTable'));;
+    }
+
+    //save table in localstorage
+    function store() {
+        window.localStorage.localTable = document.getElementById('table-body').innerHTML;
+    }
+
+    
+    //create table body rows    
+    function addData(ajax_data) {       
+        $.each(ajax_data, function (index, val) {
+            var tbl = '';
+            //add data
+            tbl += '<tr>';
+            tbl += '<td ><div class="row_data" edit_type="click" col_name="name">' + val['name'] + '</div></td>';
+            tbl += '<td ><div class="row_data" edit_type="click" col_name="value">' + val['value'] + '</div></td>';
+            //add delete button
+            tbl += '<td>';
+            tbl += '<button class="btn btn-danger delete-row">Delete</button>';
+            tbl += '</td>';
+
+            tbl += '</tr>';
+            $("#table-body").append(tbl);
+        });
+        store();
+        
+    }
+
+    //make table-cell editable
+    $(document).on('click', '.row_data', function (event) {
+        event.preventDefault();
+
+        if ($(this).attr('edit_type') == 'button') {
+            return false;
+        }
+
+        //make div editable
+        $(this).closest('div').attr('contenteditable', 'true');
+        //add bg css
+        $(this).addClass('bg-light');
+
+        $(this).focus();
+    })
+    //save table cell data
+    $(document).on('focusout', '.row_data', function (event) {
+        event.preventDefault();
+
+        if ($(this).attr('edit_type') == 'button') {
+            return false;
+        }
+
+        var row_id = $(this).closest('tr').attr('row_id');
+
+        var row_div = $(this)
+            .removeAttr('contenteditable') //make div editable		
+            .removeClass('bg-light') //add bg css
+        store();
+    })
+
+    //Add a new row
+    $('.add-row').click(function () {
+        $('#myTable tbody').append('<tr><td><div class="row_data" edit_type="click" col_name="name">click to edit</div></td><td><div class="row_data" edit_type="click" col_name="value">click to edit</div></td><td><button class="btn btn-danger delete-row">Delete</button></td></tr>');
+        store();
+    });
+
+    //delete the row
+    $(document).on('click', '.delete-row', function () {
+        $(this).parent().parent().remove();
+        store();
+    });
+
+    //select multiple files
+    function handleFileSelect(evt) {
+        var files = evt.target.files; // FileList object
+
+        // files is a FileList of File objects
+        for (var i = 0, f; f = files[i]; i++) {
+            if (f) {
+                var r = new FileReader();
+                r.onload = function (e) {
+                    //read data from file and add to table                  
+                    addData(JSON.parse(e.target.result));
+                }
+                r.readAsText(f);
+            } else {
+                alert("Failed to load file");
+            }
+        }
+    }
+    document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+
+    //create json file
+    function makeTextFile(text) {
+        var textFile = null;
+        var data = new Blob([JSON.stringify(text)], { type: 'text/json' });
+
+        // If we are replacing a previously generated file we need to
+        // manually revoke the object URL to avoid memory leaks.
+        if (textFile !== null) {
+            window.URL.revokeObjectURL(textFile);
+        }
+
+        textFile = window.URL.createObjectURL(data);
+
+        return textFile;
+    };
+
+    var create = document.getElementById('create');
+    
+
+    create.addEventListener('click', function () {
+        //converts table to JSON ignoring 3rd row | --> https://www.github.developerdan.com/table-to-json/ <--
+        var textbox = $('#myTable').tableToJSON();
+
+        var link = document.getElementById('downloadlink');        
+        link.href = makeTextFile(textbox);
+        link.setAttribute('href', link.href);
+        link.setAttribute('download', 'data.json');
+        link.classList.remove("disabled");
+    }, false);
+}); 
