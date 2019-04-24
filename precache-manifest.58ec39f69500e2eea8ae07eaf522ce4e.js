df47b88ad1e07b932fc1a5b8e11057d1c28aa039
bohdan bershadskyi <you@example.com>
2019-04-24 12:50:54 +0300

    Updates

---
+++ precache-manifest.58ec39f69500e2eea8ae07eaf522ce4e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1a40237644718b5333f",
+    "url": "/static/css/4.30efe102.chunk.css"
+  },
+  {
+    "revision": "d8993d6cc13fd4e07f7f",
+    "url": "/static/css/main.9c061709.chunk.css"
+  },
+  {
+    "revision": "9c8dd3a1acae970d42a3",
+    "url": "/static/js/runtime~main.2f9be4b6.js"
+  },
+  {
+    "revision": "7d8ff3fb844124ed0ce1",
+    "url": "/static/js/2.03e940da.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "8fbb43e179f788180472",
+    "url": "/static/js/3.a23d230f.chunk.js"
+  },
+  {
+    "revision": "d8993d6cc13fd4e07f7f",
+    "url": "/static/js/main.6a4e10bc.chunk.js"
+  },
+  {
+    "revision": "f1a40237644718b5333f",
+    "url": "/static/js/4.1ac378a0.chunk.js"
+  },
+  {
+    "revision": "47ef5c397daf33bbf55e",
+    "url": "/static/js/5.ebebf3d3.chunk.js"
+  },
+  {
+    "revision": "47ef5c397daf33bbf55e",
+    "url": "/static/css/5.00d71f98.chunk.css"
+  },
+  {
+    "revision": "8fbb43e179f788180472",
+    "url": "/static/css/3.0117628a.chunk.css"
+  },
+  {
+    "revision": "82ce47ba6789b5e30d11b54431cb850d",
+    "url": "/index.html"
+  }
+];
