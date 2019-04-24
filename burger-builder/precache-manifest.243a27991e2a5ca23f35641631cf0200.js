34ccc8efe932f3b579c6b3166f7842088ce835f2
bohdan bershadskyi <you@example.com>
2019-04-25 00:50:39 +0300

    burger builder

---
+++ burger-builder/precache-manifest.243a27991e2a5ca23f35641631cf0200.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1a40237644718b5333f",
+    "url": "/burger-builder/static/css/4.30efe102.chunk.css"
+  },
+  {
+    "revision": "f741fc5dcb3989569c68",
+    "url": "/burger-builder/static/css/main.a88695f7.chunk.css"
+  },
+  {
+    "revision": "da6bc86907c56ed7e648",
+    "url": "/burger-builder/static/js/runtime~main.5595a7b8.js"
+  },
+  {
+    "revision": "bca80921c40e70686bfd",
+    "url": "/burger-builder/static/js/2.f2787377.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "8fbb43e179f788180472",
+    "url": "/burger-builder/static/js/3.a23d230f.chunk.js"
+  },
+  {
+    "revision": "f741fc5dcb3989569c68",
+    "url": "/burger-builder/static/js/main.b49319cd.chunk.js"
+  },
+  {
+    "revision": "f1a40237644718b5333f",
+    "url": "/burger-builder/static/js/4.1ac378a0.chunk.js"
+  },
+  {
+    "revision": "47ef5c397daf33bbf55e",
+    "url": "/burger-builder/static/js/5.ebebf3d3.chunk.js"
+  },
+  {
+    "revision": "47ef5c397daf33bbf55e",
+    "url": "/burger-builder/static/css/5.00d71f98.chunk.css"
+  },
+  {
+    "revision": "8fbb43e179f788180472",
+    "url": "/burger-builder/static/css/3.0117628a.chunk.css"
+  },
+  {
+    "revision": "951d66ec271386fc9a282a098cc63a41",
+    "url": "/burger-builder/index.html"
+  }
+];
